7c0106ffa02586e74633c5768b432bbf1a1a3c18
AryanCCTech <aryan.yadav@cctech.co.in>
2024-10-15 19:55:39 +0530

    Add project files.

---
+++ IOandLoop/Header/Input.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <iostream>
+class Input
+{
+	void print();
+};
+
---
+++ IOandLoop/Source/Input.cpp
@@ -0,0 +1,7 @@
+#include "Input.h"
+
+using namespace std;
+void Input::print()
+{
+	cout << "This is the printer\n";
+}
